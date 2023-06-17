e025eb2355f7be580bbf6116c3179e4d5c6d2cb2
ShuMAhd <shuman@zykaika.ru>
2023-06-17 10:23:39 +0800

    Класс Main

--- src/main/java/ru/shum/Main.java
+++ src/main/java/ru/shum/Main.java
@@ -1,8 +1,24 @@
 package ru.shum;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Главный класс программы.
+ */
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+// Создание списка сотрудников
+    List<Employee> employees = new ArrayList<>();
+    employees.add(new Employee("John Doe", 3000));
+    employees.add(new Manager("Jane Smith", 5000));
+    employees.add(new Employee("Mark Johnson", 4000));
+
+    System.out.println("Before salary increase:");
+    for (Employee employee : employees
+    ) {
+      System.out.println(employee);
+    }
   }
 }
